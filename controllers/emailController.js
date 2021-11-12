8c1b8e8b939c2116706163abac3d9fca3aca2279
Toni Gashi <toni.gashi@sciant.com>
2021-11-12 11:23:45 +0200

    [GF-27] Initial implementation of email delivery

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,45 +1,35 @@
-const transporter = require('../nodemailer/transporter')
+const transporter = require("../nodemailer/transporter");
 
 const emailController = {
+    
+    async inviteUserToProject(receiverEmail, sender, project) {
+        let message = await transporter.sendMail({
+            from: `"${sender.firstName} ${sender.lastName} 👻" <${sender.email}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
+            to: receiverEmail, // list of receivers
+            subject: "Message Delivery Works ✔", // Subject line
+            text: `Hello, you have been invited to this project: ${project.name}`, // plain text body
+            html: `<b>Hello</b>. You have been invited to this project: ${project.name}`, // html body
+        }); 
 
-  async inviteUserToProject(receiverEmail, sender, project) {
-    let message = await transporter.sendMail({
-      from: `"${sender.firstName} ${sender.lastName} 👻" <${sender.email}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
-      to: receiverEmail, // list of receivers
-      subject: 'Message Delivery Works ✔', // Subject line
-      text: `Hello, you have been invited to this project: ${project.name}`, // plain text body
-      html: `<b>Hello</b>. You have been invited to this project: ${project.name}`, // html body
-    })
+        console.log("Message sent: %s", message.messageId);
+    },
 
-    console.log('Message sent: %s', message.messageId)
-  },
+    inviteUserToSite() {
 
-  inviteUserToSite() {
+    },
 
-  },
+    async contact(reqBody) {
+        if(!process.env.NODEMAILER_EMAIL)
+            throw 'NODEMAILER_EMAIL not set in the environment vars.'
+        let message = await transporter.sendMail({
+            from: `"${reqBody.fullName} 👻" <${reqBody.email}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
+            to: process.env.NODEMAILER_EMAIL, // list of receivers
+            subject: "Message Delivery Works ✔", // Subject line
+            text: `${reqBody.message}`, // plain text body
+        }); 
 
-  async contact(request, response) {
-    const reqBody = request.body
-
-    try{
-
-      await transporter.sendMail({
-        from: `"${reqBody.fullName} 👻" <${reqBody.email}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
-        to: process.env.NODEMAILER_EMAIL, // list of receivers
-        subject: 'Message Delivery Works ✔', // Subject line
-        text: `${reqBody.message}`, // plain text body
-      })
-
-      return response
-        .status(200)
-        .send({ message: 'success' })
-    }catch(err){
-      return response
-        .status(400)
-        .send({ message: 'email not sent!', email: process.env.NODEMAILER_EMAIL , err })
+        console.log("Message sent: %s", message.messageId);
     }
-  }
-
 }
 
-module.exports = emailController
+module.exports = emailController;
--- nodemailer/transporter.js
+++ nodemailer/transporter.js
@@ -1,15 +1,11 @@
-const nodemailer = require('nodemailer')
-
+const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
-  secure: true,
-  port: 465,
-  auth: { // TODO: google uses this to override sender email
-    user: process.env.NODEMAILER_EMAIL,
-    pass: process.env.NODEMAILER_PASS
-  },
-})
+    service: "gmail",
+    auth: { // TODO: google uses this to override sender email
+        user: "usingthisfornodemailer@gmail.com", 
+        pass: "%justatemppass"
+    },
+}); 
 
 module.exports = transporter
