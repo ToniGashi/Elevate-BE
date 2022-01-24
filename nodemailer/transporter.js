d51ceacab36cfb626a06d2a0fd8492c53c1d7772
l4z0-space <lazaronshyta@gmail.com>
2022-01-24 22:34:00 +0200

    Change nodemailer settings

--- nodemailer/transporter.js
+++ nodemailer/transporter.js
@@ -1,12 +1,15 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 
 
 let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: { // TODO: google uses this to override sender email
-        user: process.env.NODEMAILER_EMAIL, 
-        pass: process.env.NODEMAILER_PASS
-    },
-}); 
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  secure: true,
+  port: 465,
+  auth: { // TODO: google uses this to override sender email
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_PASS
+  },
+})
 
 module.exports = transporter
