08fe45ab47110c774dfdd86643beac758d7801e9
Toni Gashi <toni.gashi@sciant.com>
2022-01-24 17:34:24 +0200

    Fixing problem with envirionment variables

--- nodemailer/transporter.js
+++ nodemailer/transporter.js
@@ -1,10 +1,11 @@
 const nodemailer = require("nodemailer");
 
+
 let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: { // TODO: google uses this to override sender email
-        user: "usingthisfornodemailer@gmail.com", 
-        pass: "%justatemppass"
+        user: process.env.NODEMAILER_EMAIL, 
+        pass: process.env.NODEMAILER_PASS
     },
 }); 
 
