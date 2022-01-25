d7bbcf2e041bfc7ac1e198815887e6047ccf7e4c
Toni Gashi <toni.gashi@sciant.com>
2022-01-25 14:49:36 +0200

    Fixing edge case check

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,45 +1,48 @@
+
 const transporter = require('../nodemailer/transporter')
 
-function checkRequirements() {
-  if(!process.env.GMAIL)
-    throw 'GMAIL not set in the environment vars.'
-  if(!process.env.GMAIL_CLIENT_ID)
-    throw 'GMAIL_CLIENT_ID not set in the environment vars.'
-  if(!process.env.GMAIL_CLIENT_SECRET)
-    throw 'GMAIL_CLIENT_SECRET not set in the environment vars.'
-  if(!process.env.GMAIL_REFRESH_TOKEN)
-    throw 'GMAIL_REFRESH_TOKEN not set in the environment vars.'
-}
-
 const emailController = {
+  // async inviteUserToProject(receiverEmail, sender, project) {
+  //   checkRequirements()
+  //   let message = await transporter.sendMail({
+  //     from: `"${sender.firstName} ${sender.lastName} 👻" <${sender.email}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
+  //     to: receiverEmail, // list of receivers
+  //     subject: 'Message Delivery Works ✔', // Subject line
+  //     text: `Hello, you have been invited to this project: ${project.name}`, // plain text body
+  //     html: `<b>Hello</b>. You have been invited to this project: ${project.name}`, // html body
+  //   })
 
-  async inviteUserToProject(receiverEmail, sender, project) {
-    checkRequirements()
-    let message = await transporter.sendMail({
-      from: `"${sender.firstName} ${sender.lastName} 👻" <${sender.email}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
-      to: receiverEmail, // list of receivers
-      subject: 'Message Delivery Works ✔', // Subject line
-      text: `Hello, you have been invited to this project: ${project.name}`, // plain text body
-      html: `<b>Hello</b>. You have been invited to this project: ${project.name}`, // html body
-    })
+  //   console.log('Message sent: %s', message.messageId)
+  // },
 
-    console.log('Message sent: %s', message.messageId)
-  },
+  // inviteUserToSite() {
 
-  inviteUserToSite() {
+  // },
 
-  },
+  async contact(req, res) {
+    try {
+      const { email, message } = req.body
+      if(!email || !message) {
+        throw new Error('Bad request!')
+      }
 
-  async contact(reqBody) {
-    checkRequirements()
-    let message = await transporter.sendMail({
-      from: `"GreenFunds 👻" <${process.env.NODEMAILER_EMAIL}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
-      to: reqBody.email, // list of receivers
-      subject: 'Message Delivery Works ✔', // Subject line
-      text: `"${reqBody.message}"\nWe have received this message from you. Thank you so much for your inquiry! We will make sure to get to it as soon as possible.`, // plain text body
-    })
+      let mail = await transporter.sendMail({
+        from: `"GreenFunds 👻" <${process.env.NODEMAILER_EMAIL}>`,
+        to: email, // list of receivers
+        subject: 'Message Delivery Works ✔', // Subject line
+        text: `"${message}"\nWe have received this message from you. Thank you so much for your inquiry! We will make sure to get to it as soon as possible.`, // plain text body
+      })
 
-    console.log('Message sent: %s', message.messageId)
+      if(!mail.messageId) {
+        throw new Error('There was a problem sending the mail')
+      }
+
+      res.status(200).send({ message: 'Successfully sent email!' })
+    } catch (error) {
+      res.status(400).send({
+        error: { message: error.message }
+      })
+    }
   }
 }
 
