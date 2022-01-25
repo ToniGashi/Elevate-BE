6c13824a9e34240ffad63116a382ff66ce33f564
Toni Gashi <toni.gashi@sciant.com>
2022-01-25 13:28:02 +0200

    Fixing nodemailer service

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,35 +1,46 @@
-const transporter = require("../nodemailer/transporter");
+const transporter = require('../nodemailer/transporter')
 
-const emailController = {
-    
-    async inviteUserToProject(receiverEmail, sender, project) {
-        let message = await transporter.sendMail({
-            from: `"${sender.firstName} ${sender.lastName} 👻" <${sender.email}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
-            to: receiverEmail, // list of receivers
-            subject: "Message Delivery Works ✔", // Subject line
-            text: `Hello, you have been invited to this project: ${project.name}`, // plain text body
-            html: `<b>Hello</b>. You have been invited to this project: ${project.name}`, // html body
-        }); 
-
-        console.log("Message sent: %s", message.messageId);
-    },
-
-    inviteUserToSite() {
-
-    },
-
-    async contact(reqBody) {
-        if(!process.env.NODEMAILER_EMAIL)
-            throw 'NODEMAILER_EMAIL not set in the environment vars.'
-        let message = await transporter.sendMail({
-            from: `"${reqBody.fullName} 👻" <${reqBody.email}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
-            to: process.env.NODEMAILER_EMAIL, // list of receivers
-            subject: "Message Delivery Works ✔", // Subject line
-            text: `${reqBody.message}`, // plain text body
-        }); 
-
-        console.log("Message sent: %s", message.messageId);
-    }
+function checkRequirements() {
+  if(!process.env.GMAIL)
+    throw 'GMAIL not set in the environment vars.'
+  if(!process.env.GMAIL_CLIENT_ID)
+    throw 'GMAIL_CLIENT_ID not set in the environment vars.'
+  if(!process.env.GMAIL_CLIENT_SECRET)
+    throw 'GMAIL_CLIENT_SECRET not set in the environment vars.'
+  if(!process.env.GMAIL_REFRESH_TOKEN)
+    throw 'GMAIL_REFRESH_TOKEN not set in the environment vars.'
 }
 
-module.exports = emailController;
+const emailController = {
+
+  async inviteUserToProject(receiverEmail, sender, project) {
+    checkRequirements()
+    let message = await transporter.sendMail({
+      from: `"${sender.firstName} ${sender.lastName} 👻" <${sender.email}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
+      to: receiverEmail, // list of receivers
+      subject: 'Message Delivery Works ✔', // Subject line
+      text: `Hello, you have been invited to this project: ${project.name}`, // plain text body
+      html: `<b>Hello</b>. You have been invited to this project: ${project.name}`, // html body
+    })
+
+    console.log('Message sent: %s', message.messageId)
+  },
+
+  inviteUserToSite() {
+
+  },
+
+  async contact(reqBody) {
+    checkRequirements()
+    let message = await transporter.sendMail({
+      from: `"GreenFunds 👻" <${process.env.NODEMAILER_EMAIL}>`, // TODO: The senders email won't show because google overwrites it so we may want to switch to hotmail or something
+      to: reqBody.email, // list of receivers
+      subject: 'Message Delivery Works ✔', // Subject line
+      text: `"${reqBody.message}"\nWe have received this message from you. Thank you so much for your inquiry! We will make sure to get to it as soon as possible.`, // plain text body
+    })
+
+    console.log('Message sent: %s', message.messageId)
+  }
+}
+
+module.exports = emailController
--- nodemailer/transporter.js
+++ nodemailer/transporter.js
@@ -1,11 +1,36 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+const OAuth2 = google.auth.OAuth2
+
+const oauth2Client = new OAuth2(
+  process.env.GMAIL_CLIENT_ID,
+  process.env.GMAIL_CLIENT_SECRET,
+  'https://developers.google.com/oauthplayground'
+)
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GMAIL_REFRESH_TOKEN
+})
+
+const accessToken = new Promise((resolve, reject) => {
+  oauth2Client.getAccessToken((err, token) => {
+    if (err) {
+      reject('Failed to create access token :(')
+    }
+    resolve(token)
+  })
+})
 
 let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: { // TODO: google uses this to override sender email
-        user: "usingthisfornodemailer@gmail.com", 
-        pass: "%justatemppass"
-    },
-}); 
+  service: 'gmail',
+  auth: { // TODO: google uses this to override sender email
+    type: 'OAuth2',
+    user: process.env.GMAIL,
+    accessToken,
+    clientId: process.env.GMAIL_CLIENT_ID,
+    clientSecret: process.env.GMAIL_CLIENT_SECRET,
+    refreshToken: process.env.GMAIL_REFRESH_TOKEN
+  },
+})
 
 module.exports = transporter
